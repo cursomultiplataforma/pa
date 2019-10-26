3cdad25af652950028bec9e9afc3828971254570
Daniel Pérez <daniel.perez@urjc.es>
2019-10-26 15:43:13 +0200

    prueba 3

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const name_app = 'pa';
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/'+ name_app));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/'+ name_app +'/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
