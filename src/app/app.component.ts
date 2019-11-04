7e747458fde07e0f22e5baa402de1f787a24c43d
Daniel Pérez <daniel.perez@urjc.es>
2019-11-04 18:24:49 +0100

    prueba http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'pa';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+    const options: any = {
+      headers: new HttpHeaders(),
+      params: new HttpParams(),
+      responseType: 'json',
+      observe: 'response'
+    };
+
+    this.http.get(environment.api, options).subscribe(
+      resp => { console.log('test get ok'); },
+      error => { console.log('err get'); }
+    );
+  }
 }
